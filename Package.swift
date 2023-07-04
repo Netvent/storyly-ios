e35bf1cd7b457514188c1b1187e7794c88c0e495
Yigit <caliskanyigit@gmail.com>
2023-07-04 17:34:39 +0300

    build(storyly): release 2.0.0

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Storyly",
-            url: "https://prod-storyly-media.s3-eu-west-1.amazonaws.com/storyly-sdk/1.33.0/Storyly.zip",
-            checksum: "c8cac824941a430ccc267434af8407c0c514da8cfc38f145e60520801dccd804"
+            url: "https://prod-storyly-media.s3-eu-west-1.amazonaws.com/storyly-sdk/2.0.0/Storyly.zip",
+            checksum: "cad963b1803f860a05a9d156fcc30bb10842cfaed77920cbca9417198bdffa63"
         )
     ]
 )
